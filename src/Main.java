aab377ce2c8b8dd6343cbced5c0c733f540371bf
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-04-27 19:45:55 +0200

    1. Creating Pawns and Board photos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Chessboard");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 1000);
+        Board panel = new Board();
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
