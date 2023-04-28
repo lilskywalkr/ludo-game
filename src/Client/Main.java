423ccaa8ff40a975ecac084917b79050fe0792bb
Aveniss <kamilst934@gmail.com>
2023-04-28 15:42:50 +0200

    PC-003 Pierwsza wersja mapy

---
+++ src/Client/Main.java
@@ -0,0 +1,14 @@
+package Client;
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MyPanel Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new Board());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
