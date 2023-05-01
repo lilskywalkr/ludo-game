4cf3404410cd622c0abd90f92360ce67c46842e5
lilskywalkr <ronald.frangulyan@gmail.com>
2023-05-02 00:25:11 +0200

    added pawns

--- src/Client/Board.java
+++ src/Client/Board.java
@@ -2,12 +2,14 @@ package Client;
 
 import java.awt.*;
 import java.util.InputMismatchException;
-import javax.swing.JPanel;
+import java.util.Objects;
+import javax.swing.*;
 
 public class Board extends JPanel {
 
     private static final int SQUARE_SIZE = 50;
     private static final int CIRCLE_SIZE = 50;
+    private static final int PAWN_SIZE = 30;
     private static final int SPACE_BETWEEN_CIRCLE = 60;
     private static final int BIG_SQUARE_SIZE = 300;
     private static final float STROKE_WIDTH = 2.0f;
@@ -42,6 +44,30 @@ public class Board extends JPanel {
 
         drawColorPart(g2d);
         g2d.setStroke(oldStroke);
+
+        // Red pawn
+        drawPawn(g2d, 2, 75, 70);
+        drawPawn(g2d, 2, 195, 70);
+        drawPawn(g2d, 2, 75, 190);
+        drawPawn(g2d, 2, 195, 190);
+
+        // Green pawn
+        drawPawn(g2d, 1, 525, 70);
+        drawPawn(g2d, 1, 645, 70);
+        drawPawn(g2d, 1, 525, 190);
+        drawPawn(g2d, 1, 645, 190);
+
+        // Yellow pawn
+        drawPawn(g2d, 3, 75, 520);
+        drawPawn(g2d, 3, 195, 520);
+        drawPawn(g2d, 3, 75, 640);
+        drawPawn(g2d, 3, 195, 640);
+
+        // Blue pawn
+        drawPawn(g2d, 0, 525, 520);
+        drawPawn(g2d, 0, 645, 520);
+        drawPawn(g2d, 0, 525, 640);
+        drawPawn(g2d, 0, 645, 640);
     }
 
     private void drawColorPart(Graphics2D g2d)
@@ -108,5 +134,14 @@ public class Board extends JPanel {
         g2d.drawRect(x,y, size, size);
     }
 
+    private void drawPawn(Graphics2D g2d, int color, int x, int y) {
+        try {
+            ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("../assets/" + color + ".png")));
+            Image pawnIcon = icon.getImage();
+            g2d.drawImage(pawnIcon, x, y, PAWN_SIZE, PAWN_SIZE, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
